938ed28952c075de15f18d60533127a5a0128ac3
without eyes <37347347+without-eyes@users.noreply.github.com>
2024-12-26 11:05:19 +0200

    Add DHT11

--- WeatherStation.ino
+++ WeatherStation.ino
@@ -1,17 +1,17 @@
-const int lm35_pin = A14;	
+#include <dht.h>
+#define DHT11_PIN 7
+
+dht DHT;
 
 void setup() {
   Serial.begin(9600);
 }
 
 void loop() {
-  int temp_adc_val;
-  float temp_val;
-  temp_adc_val = analogRead(lm35_pin);	/* Read Temperature */
-  temp_val = (temp_adc_val * 4.88);	/* Convert adc value to equivalent voltage */
-  temp_val = (temp_val/10);	/* LM35 gives output of 10mv/°C */
+  int chk = DHT.read11(DHT11_PIN);
   Serial.print("Temperature = ");
-  Serial.print(temp_val);
-  Serial.print(" Degree Celsius\n");
-  delay(1000);
+  Serial.print(DHT.temperature);
+  Serial.print(";   Humidity = ");
+  Serial.println(DHT.humidity);
+  delay(10000);
 }
